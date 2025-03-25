b7f3754a8212ef50f623317443fe099f746b6b03
Omar Hazem <omarhazem6311@gmail.com>
2025-03-25 23:37:27 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// function loveCalc (name1 , name2){
+//     var n = Math.random()*100;
+//     n = Math.floor(n) + 1;
+//     alert(name1 + " & " + name2 +" = "+ n);
+// }
+// var name1=  prompt("enter your name,");
+// var name2 = prompt("enter your Crush name ,");
+// loveCalc(name1 , name2);
